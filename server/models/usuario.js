9d046cde3b0139479aba9978eec51797771a4952
Rafael Estrada <restradap1600@gmail.com>
2020-11-18 11:03:10 -0600

    Se encriptó la contraseña con bcrypt y se ocultó

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -40,6 +40,14 @@ let usuarioSchema = new Schema({
     }
 });
 
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
 usuarioSchema.plugin(uniqueValidator, {message: '{PATH} debe ser unico'});
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
