656a9a37fadf8c1f24679e475eacaf38bae4a263
Rafael Estrada <restradap1600@gmail.com>
2020-11-17 19:27:20 -0600

    Se añadió Mongoose al proyecto

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 require("./config/config");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
@@ -38,6 +42,11 @@ app.put("/usuario/:id", (req, res) => {
 
 app.delete("/usuario", (req, res) => {
     res.json("delete /usuario");
+});
+
+mongoose.connect("mongodb://localhost:27017/dbCafe", (err, res) => {
+    if (err) throw err;
+    console.log("base de datos conectada");
 });
 
 app.listen(process.env.PORT, () => {
